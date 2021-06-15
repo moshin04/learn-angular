dcdf4ef3b73f95b1a90acca9547a650050ca9c26
mohashin g <mohashin.gachinmal@vernost.in>
2021-06-15 15:09:56 +0530

    events through--> used click,doubleclick,focus,blur,keyup,mouseenter events

--- src/app/eventbind/eventbind.component.ts
+++ src/app/eventbind/eventbind.component.ts
@@ -16,20 +16,44 @@ export class EventbindComponent implements OnInit {
   
   cartmsg="";
 
+
   myAddCart(){
 
-      this.msg = "Added to CART"
+      this.msg = " Added to CART"
   }
 
-
-
   myInputValue(event: any){
-    console.log(event.target.value);
+    console.log(event);
 
   }
 
   addCart(event: any){
-    this.cartmsg = event.target.value + "Added in Cart"
+    this.cartmsg = event.target.value + " Added in Cart"
   }
+
+  focusCallback(){
+    console.log('focusCallback');
+
+  }
+  blurCallback(){
+    console.log('blurCallback')
+  }
+
+  clickCallback(){
+    console.log('clickCallback')
+  }
+
+  dblclickCallback(){
+    console.log('dblclickCallback')
+  }
+
+  keydownCallback(){
+    console.log('KeyPressed');
+  }
+
+  mouseenterCallback(){
+    console.log('mouse entered')
+  }
+
 
 }
